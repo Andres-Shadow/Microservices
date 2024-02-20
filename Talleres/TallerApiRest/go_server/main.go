2a83b16f5c009e0ed46831f52a5e1ff3ffe3a6fb
andres-shadow <andresm.dussanb@uqvirtual.edu.co>
2024-02-20 12:51:30 -0500

    taller 2 punto 3 semi ok

--- Talleres/TallerApiRest/go_server/main.go
+++ Talleres/TallerApiRest/go_server/main.go
@@ -12,6 +12,7 @@ import (
 
 func main() {
 
+	//establish a database connection
 	DataBase.DBConnection()
 	DataBase.DB.AutoMigrate(models.User{})
 	r := mux.NewRouter()
@@ -19,5 +20,20 @@ func main() {
 	//login route
 	r.HandleFunc("/login", handlers.HomeHandler).Methods("POST")
 
-	http.ListenAndServe(":9093", r)
+	//user routes
+	//creating route prefix
+	//and delegating a function subroutes responsability
+	defineUserEndpoints(r.PathPrefix("/api/v1/user").Subrouter())
+
+	http.ListenAndServe(":9090", r)
+}
+
+
+//defineUserEndpoints is a function that defines the user subroutes
+//user the prefix "/api/v1/user"
+func defineUserEndpoints(userRouter *mux.Router) {
+	userRouter.HandleFunc("/all", handlers.GetUsersHandler).Methods("GET")
+	userRouter.HandleFunc("/add", handlers.PostUserHandler).Methods("POST")
+	userRouter.HandleFunc("/delete", handlers.DeleteUserHandler).Methods("DELETE")
+	userRouter.HandleFunc("/update", handlers.PostUserHandler).Methods("PUT")
 }
