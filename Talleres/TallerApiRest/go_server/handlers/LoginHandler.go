004c6d0ce2ef744e1202e56cc15ca31d9f5044bf
andres-shadow <andresm.dussanb@uqvirtual.edu.co>
2024-02-22 23:00:31 -0500

    punto 4 auth ready

--- Talleres/TallerApiRest/go_server/handlers/LoginHandler.go
+++ Talleres/TallerApiRest/go_server/handlers/LoginHandler.go
@@ -2,9 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"taller_apirest/models"
+	"taller_apirest/security"
 	"taller_apirest/utilities"
 )
 
@@ -25,6 +27,13 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Verificar si la solicitud es de tipo POST
+	if r.Method != http.MethodPost {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
 	//decode user from json
 	var user models.User
 	json.NewDecoder(r.Body).Decode(&user)
@@ -36,6 +45,15 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//user creation success
-	json.NewEncoder(w).Encode(&user)
+	// Verificar si se proporcionaron usuario y clave
+	if user.Username == "" || user.Password == "" {
+		http.Error(w, "Faltan usuario y claves", http.StatusBadRequest)
+		return
+	}
+
+	tokenString := security.LoginHandler(&user)
+
+	// Responder con el token JWT
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprint(w, tokenString)
 }
--- Talleres/TallerApiRest/go_server/security/Security.go
+++ Talleres/TallerApiRest/go_server/security/Security.go
@@ -1,9 +1,6 @@
 package security
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
 	"taller_apirest/models"
 	"time"
 
@@ -11,42 +8,17 @@ import (
 )
 
 // Manejador para la ruta /login
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	// Verificar si la solicitud es de tipo POST
-	if r.Method != http.MethodPost {
-		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
-		return
-	}
-
-	// Parsear el cuerpo de la solicitud para obtener las credenciales
-	var creds models.User
-	err := json.NewDecoder(r.Body).Decode(&creds)
-	if err != nil {
-		http.Error(w, "Error al leer las credenciales", http.StatusBadRequest)
-		return
-	}
-
-	// Verificar si se proporcionaron usuario y clave
-	if creds.Username == "" || creds.Password == "" {
-		http.Error(w, "Faltan usuario y claves", http.StatusBadRequest)
-		return
-	}
+func LoginHandler(user *models.User) string {
 
 	// Generar el token JWT
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
-	claims["sub"] = creds.Username
+	claims["sub"] = user.Username
 	claims["exp"] = time.Now().Add(time.Hour).Unix() // Token válido por una hora
 	claims["iss"] = "ingesis.uniquindio.edu.co"
 
 	// Firmar el token con una clave secreta y obtener el string del token
-	tokenString, err := token.SignedString([]byte("contraseña_super_secreta_100%_real_no_fake"))
-	if err != nil {
-		http.Error(w, "Error al firmar el token", http.StatusInternalServerError)
-		return
-	}
+	tokenString, _ := token.SignedString([]byte("contraseña_super_secreta_100%_real_no_fake"))
 
-	// Responder con el token JWT
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprint(w, tokenString)
+	return tokenString
 }
