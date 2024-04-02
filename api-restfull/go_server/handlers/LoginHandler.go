e4d3cbd9c043419c0076adf826897325f3428d47
andres-shadow <andresm.dussanb@uqvirtual.edu.co>
2024-04-02 12:42:40 -0500

    taller 4

---
+++ api-restfull/go_server/handlers/LoginHandler.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"taller_apirest/models"
+	"taller_apirest/security"
+	"taller_apirest/utilities"
+)
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Verificar si la solicitud es de tipo POST
+	if r.Method != http.MethodPost {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	//decode user from json
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+	_, err := utilities.SearchUser(&user)
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("User not found"))
+		return
+	}
+
+	// Verificar si se proporcionaron usuario y clave
+	if user.Username == "" || user.Password == "" {
+		http.Error(w, "Faltan usuario y claves", http.StatusBadRequest)
+		return
+	}
+
+	tokenString := security.LoginHandler(&user)
+
+	// Responder con el token JWT
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprint(w, tokenString)
+}
