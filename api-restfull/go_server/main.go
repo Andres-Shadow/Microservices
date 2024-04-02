758dc69f61d4e630aad68a69b97a230ae74e915c
andres-shadow <andresm.dussanb@uqvirtual.edu.co>
2024-04-02 12:42:40 -0500

    taller 4

---
+++ api-restfull/go_server/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"net/http"
+
+	DataBase "taller_apirest/Database"
+	"taller_apirest/handlers"
+	"taller_apirest/models"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	//establish a database connection
+	DataBase.DBConnection()
+	DataBase.DB.AutoMigrate(models.User{})
+	r := mux.NewRouter()
+
+	//cargado de datos de prueba
+	users := []models.User{
+		{Username: "pepe", Email: "a@gmail.com", Password: "12345"},
+		{Username: "pepe2", Email: "b@gmail.com", Password: "12345"},
+		{Username: "pepe3", Email: "c@gmail.com", Password: "12345"},
+		{Username: "pepe4", Email: "d@gmail.com", Password: "12345"},
+		{Username: "pepe5", Email: "e@gmail.com", Password: "12345"},
+		{Username: "pepe6", Email: "f@gmail.com", Password: "12345"},
+	}
+
+	for _, user := range users {
+		DataBase.DB.Create(&user)
+	}
+
+	//login route
+	defineLoginRegisterEndpoints(r.PathPrefix("/api/v1").Subrouter())
+
+	//user routes
+	//creating route prefix
+	//and delegating a function subroutes responsability
+	defineUserEndpoints(r.PathPrefix("/api/v1/users").Subrouter())
+
+	http.ListenAndServe(":9090", r)
+}
+
+// defineUserEndpoints is a function that defines the user subroutes
+// user the prefix "/api/v1/user"
+func defineUserEndpoints(userRouter *mux.Router) {
+	//RESTful API endpoints for crud
+	userRouter.HandleFunc("/", handlers.GetUsersHandler).Methods("GET")
+	userRouter.HandleFunc("/{id}", handlers.GetUserHandlerById).Methods("GET")
+	userRouter.HandleFunc("/", handlers.PostUserHandler).Methods("POST")
+	userRouter.HandleFunc("/", handlers.DeleteUserHandler).Methods("DELETE")
+	userRouter.HandleFunc("/", handlers.UpdateUserHandler).Methods("PUT")
+
+	//RESTful API endpoints for user recover and update password
+	userRouter.HandleFunc("/password/", handlers.RecoverPassword).Methods("GET")
+	userRouter.HandleFunc("/password", handlers.UpdateUserPassword).Methods("PATCH")
+}
+
+func defineLoginRegisterEndpoints(loginRouter *mux.Router) {
+	loginRouter.HandleFunc("/login", handlers.LoginHandler).Methods("POST")
+}
