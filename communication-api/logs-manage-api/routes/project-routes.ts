4cc059165ba985986dbf2af4897f473de5e5a17f
andres-shadow <andresm.dussanb@uqvirtual.edu.co>
2024-04-10 19:36:01 -0500

    taller 4 traduccion a ts

---
+++ communication-api/logs-manage-api/routes/project-routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { getLog, createLog } from "../handlers/logs-handler"; 
+
+const router = Router();
+
+
+router.get('/api/v1/logs/', getLog)
+router.post('/api/v1/logs/', createLog)
+
+export default router;
