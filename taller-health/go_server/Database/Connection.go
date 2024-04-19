da6d2b017636dc771efb27c20565f669f993045d
andres-shadow <andresm.dussanb@uqvirtual.edu.co>
2024-04-18 20:23:27 -0500

    blindado conexion a base de datos

--- taller-health/go_server/Database/Connection.go
+++ taller-health/go_server/Database/Connection.go
@@ -3,13 +3,11 @@ package DataBase
 import (
 	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
-
-// comando base de datos
-// docker run --name postgres -e POSTGRES_USER=andres -e POSTGRES_PASSWORD=1234 -p 5432:5432 -d postgres
 
 var DB *gorm.DB
 
@@ -19,13 +17,18 @@ func DBConnection() {
 	if host == "" {
 		host = "localhost"
 	}
-	
+
 	var DSN = "host=" + host + " user=andres password=1234 dbname=users port=5432"
-	var error error
-	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
-	if error != nil {
-		log.Fatal(error)
-	} else {
-		log.Println("BD Connected")
+
+	for {
+		var err error
+		DB, err = gorm.Open(postgres.Open(DSN), &gorm.Config{})
+		if err != nil {
+			log.Println("Failed to connect to database. Retrying in 5 seconds...")
+			time.Sleep(5 * time.Second) // Wait for 5 seconds before retrying
+		} else {
+			log.Println("DB Connected")
+			break // Exit the loop once the connection is successful
+		}
 	}
 }
