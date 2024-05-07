849cf7d9ffdd8cd05050f6f56bc9f72c3614ec79
andres-shadow <andresm.dussanb@uqvirtual.edu.co>
2024-05-06 22:22:22 -0500

    taller 6 gateway auth api

--- api_gateway/gateway/server.rb
+++ api_gateway/gateway/server.rb
@@ -61,9 +61,13 @@ def iniciar_servidor
   servidor = WEBrick::HTTPServer.new(Port: 9095)
 
   # Ruta para redirigir a la operación de autenticación
-  servidor.mount_proc '/api/v1/users' do |req, res|
-    redirigir_solicitud("http://localhost:9090/api/v1/users/", req, res)
-  end
+    servidor.mount_proc '/api/v1/users' do |req, res|
+        redirigir_solicitud("http://localhost:9090/api/v1/users/", req, res)
+    end
+
+    servidor.mount_proc '/api/v1/users/login' do |req, res|
+        redirigir_solicitud("http://localhost:9090/api/v1/login", req, res)
+    end
 
   # Manejar la señal de interrupción para detener el servidor
   trap 'INT' do
